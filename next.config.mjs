9e59a904dcc987f4cefcb1ce9c7f593102a6b839
DicoAji <dicoaji12@gmail.com>
2024-06-06 16:03:23 +0700

    upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  assetPrefix: isProd ? "/next.portfolio/" : "",
-  basePath: isProd ? "/next.portfolio" : "",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  output: "export",
-};
+module.exports = {};
